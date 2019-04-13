638688bb87d3ede349209806f725efcbfacc833a
gordon yip <>
2019-04-13 18:02:00 +0800

    typo

---
+++ precache-manifest.70dbca206fd3dd110daab7992c815b5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1dc6b93094aad43df4e0",
+    "url": "/static/js/main.ce347469.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "1dc6b93094aad43df4e0",
+    "url": "/static/css/main.248cb6a6.chunk.css"
+  },
+  {
+    "revision": "5f17a6830ad0cac38fc86faecc88dc90",
+    "url": "/index.html"
+  }
+];
